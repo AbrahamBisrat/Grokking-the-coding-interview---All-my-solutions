511294a1131711866ec38995b3813238a170241b
Abraham Bisrat <abrahammehari0@gmail.com>
2022-07-04 18:00:35 -0500

    Added reverse order traversal of bst and more testing data

--- src/breadthFirstSearch/LevelOrderTraversalBST.java
+++ src/breadthFirstSearch/LevelOrderTraversalBST.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class LevelOrderTraversalBST {
-	class TreeNode {
+	static class TreeNode {
 		private TreeNode left;
 		private TreeNode right;
 		private int val;
-		public TreeNode(int thatVal, TreeNode left, TreeNode right) {
+		TreeNode(int thatVal) {
 			this.val = thatVal;
-			this.right = right;
-			this.left = left;
 		}
 	}
-	public List<List<Integer>> levelOrder(TreeNode root) {
+	public static List<List<Integer>> levelOrder(TreeNode root) {
 		List<List<Integer>> allLevels = new ArrayList<>();
 		if(root == null) return allLevels;
 		
@@ -41,5 +40,43 @@ public class LevelOrderTraversalBST {
 			allLevels.add(levelData);
 		}
 		return allLevels;
+	}
+	public static List<List<Integer>> reverseLevelOrder(TreeNode root) {
+		List<List<Integer>> allReverseLevels = new ArrayList<>();
+		if(root == null) return allReverseLevels;
+		
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+		
+		Stack<List<Integer>> reverseOrderStack = new Stack<>();
+		
+		while(!queue.isEmpty()) {
+			int levelSize = queue.size();
+			List<Integer> levelData = new ArrayList<>();
+			while(levelSize-- > 0) {
+				TreeNode currentNode = queue.poll();
+				levelData.add(currentNode.val);
+				
+				if(currentNode.right != null)
+					queue.add(currentNode.right);
+				if(currentNode.left != null)
+					queue.add(currentNode.left);
+			}
+			reverseOrderStack.push(levelData);
+		}
+		while(!reverseOrderStack.isEmpty())
+			allReverseLevels.add(reverseOrderStack.pop());
+		return allReverseLevels;
+	}
+	static void p(Object line) { System.out.println(line); }
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(12);
+		root.left = new TreeNode(7);
+		root.right = new TreeNode(1);
+		root.left.left = new TreeNode(9);
+		root.right.left = new TreeNode(10);
+		root.right.right = new TreeNode(5);
+		p(levelOrder(root));
+		p(reverseLevelOrder(root));
 	}
 }
