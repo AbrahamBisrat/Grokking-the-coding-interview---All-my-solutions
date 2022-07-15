bca595e8b2dfe1f83ee5c72a5ec2092bfdcc1dc0
Abraham Bisrat <abrahammehari0@gmail.com>
2022-07-15 17:16:22 -0500

    Added find missing number via cyclic sort

--- src/cyclicSort/FindMissingNumber.java
+++ src/cyclicSort/FindMissingNumber.java
@@ -6,17 +6,17 @@ import java.util.Arrays;
  * 	Problem Statement #
 	We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’.
 	Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
-
+	
 	Example 1:
 	Input: [4, 0, 3, 1]
 	Output: 2
-
+	
 	Example 2:
 	Input: [8, 3, 5, 2, 4, 6, 0, 1]
 	Output: 7
  * 
  * @author Er. Abraham Bisrat https://github.com/abrahammehari
- *
+ * 
  */
 public class FindMissingNumber {
 	public static void p(Object line) { System.out.println(line); }
@@ -27,8 +27,21 @@ public class FindMissingNumber {
 		p(findMissingNumber(new int[] {8, 3, 5, 2, 4, 6, 0, 1}));
 	}
 	public static int findMissingNumber(int[] arr) {
+		int index = 0;
 		
+		while(index < arr.length)
+			if(arr[index] != index && arr[index] < arr.length) swap(arr, index, arr[index]);
+			else index++;
 		
-		return 0;
+		for(int i = 0; i < arr.length; i++)
+			if(arr[i] != i)
+				return i;
+		
+		return arr.length;
+	}
+	public static void swap(int[] arr, int from, int to) {
+		int temp = arr[from];
+		arr[from] = arr[to];
+		arr[to] = temp;
 	}
 }
