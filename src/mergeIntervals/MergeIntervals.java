c7208a6ba4b51e21d378a1be061336cbe7a6e337
Abraham Bisrat <abrahammehari0@gmail.com>
2022-07-15 23:11:33 -0500

    Added array version of merge intervals

--- src/mergeIntervals/MergeIntervals.java
+++ src/mergeIntervals/MergeIntervals.java
@@ -73,6 +73,34 @@ public class MergeIntervals {
 		mergedIntervals.add(new Interval(start, end));
 		return mergedIntervals;
 	}
+	// Array version
+	public int[][] merge(int[][] intervals) {
+        if(intervals.length < 2) return intervals;
+        
+        // sorting by start of the interval is critical to the whole operation
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        List<int[]> mergedIntervals = new ArrayList<>();
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        for(int i = 1; i < intervals.length; i++) {
+            int[] current = intervals[i];
+            if(current[0] <= end) end = Math.max(current[1], end);
+            else {
+                mergedIntervals.add(new int[] {start, end});
+                start = current[0];
+                end = current[1];
+            }
+        }
+        
+        mergedIntervals.add(new int[] {start, end});
+        
+        int[][] mergedResult = new int[mergedIntervals.size()][2];
+        for(int i = 0; i < mergedIntervals.size(); i++)
+            mergedResult[i] = mergedIntervals.get(i);
+        return mergedResult;
+    }
 }
 
 
